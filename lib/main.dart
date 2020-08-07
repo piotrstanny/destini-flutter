3be200aefcb130ccf9eb525a04086e48751d4403
Piotr Stanny <piotr.stanny@gmail.com>
2020-08-07 17:22:10 +0100

    Implement conditions to progress through the story displaying next stages accordingly

--- lib/main.dart
+++ lib/main.dart
@@ -50,8 +50,10 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 child: FlatButton(
                   onPressed: () {
-                    //Choice 1 made by user.
-                    storyBrain.nextStory(1);
+                    setState(() {
+                      //Choice 1 made by user.
+                      storyBrain.nextStory(1);
+                    });
                   },
                   color: Colors.red,
                   child: Text(
@@ -72,8 +74,10 @@ class _StoryPageState extends State<StoryPage> {
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
                 child: FlatButton(
                   onPressed: () {
-                    //Choice 2 made by user.
-                    storyBrain.nextStory(2);
+                    setState(() {
+                      //Choice 2 made by user.
+                      storyBrain.nextStory(2);
+                    });
                   },
                   color: Colors.blue,
                   child: Text(
@@ -92,7 +96,5 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
 
 //TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
