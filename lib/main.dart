51e3bf9552bcdf59f38cc26316380ec2e6d3cb2b
Piotr Stanny <piotr.stanny@gmail.com>
2020-08-07 14:08:10 +0100

    Create getStory function to display a first story

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class Destini extends StatelessWidget {
   }
 }
 
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+StoryBrain storyBrain = StoryBrain();
 
 class StoryPage extends StatefulWidget {
   _StoryPageState createState() => _StoryPageState();
@@ -41,10 +41,9 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
+                    storyBrain.getStory(),
                     style: TextStyle(
-                      fontSize: 25.0,
+                      fontSize: 24.0,
                     ),
                   ),
                 ),
