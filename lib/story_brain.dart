691d2cbbaca50c73c7a37f7b1d910020876a4f65
Piotr Stanny <piotr.stanny@gmail.com>
2020-08-08 13:42:03 +0100

    Implement method to control visibility of the blue button, hiding when game is finished

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -47,8 +47,6 @@ class StoryBrain {
     return _storyData[_storyNumber].choice2;
   }
 
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
   void nextStory(int choiceNo) {
     if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
       restartStory();
@@ -77,5 +75,11 @@ class StoryBrain {
     _storyNumber = 0;
   }
 
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
